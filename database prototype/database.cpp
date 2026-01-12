d95a5fb4218cacc13a9d91468ca5fc0106c2d6af
vinn044 <vincek1800@gmail.com>
2026-01-12 03:13:27 -0500

    basic input then saving

--- database prototype/database.cpp
+++ database prototype/database.cpp
@@ -4,9 +4,21 @@
 using namespace std;
 
 int main() {
+    int id;
+    string name;
+    int age;
 
 
+    cout << "Enter ID: ";
+    cin >> id;
 
+    cout << "Enter Name: ";
+    cin >> name;
 
-    cout << "placeholder: ";
+    cout << "Enter Age: ";
+    cin >> age;
+
+    return 0;
+
+    
 }
